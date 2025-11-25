32792a3d1d2f5cade219cdc682c72d89d5344570
Mikhail Pavlov <i.spiori@gmail.com>
2025-11-25 22:42:53 +0500

    Добавлен скриншот тесты, переименован класс таббара для соответствия структуре файлов

--- Tracker/App/SceneDelegate.swift
+++ Tracker/App/SceneDelegate.swift
@@ -11,7 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MainTabBarController()
+        window.rootViewController = TabBarViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
--- Tracker/Presentation/Screens/TabBar/TabBarViewController.swift
+++ Tracker/Presentation/Screens/TabBar/TabBarViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class MainTabBarController: UITabBarController {
+final class TabBarViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
